b93cc7dfcf45ec118b008c65d1c46bf0733ebfd3
Johudo <dimazuev11@gmail.com>
2021-10-03 15:01:43 +0300

    Added URLs in .env

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,5 +1,5 @@
-export const BACKEND_API_URL = "http://127.0.0.1:8000";
-export const NEXT_API_URL = "http://localhost:3000";
+export const BACKEND_API_URL = process.env.DJANGO_URL || "";
+export const NEXT_API_URL = process.env.NEXT_URL || "";
 export const JWT_AUTH_HEADER_PREFIX = process.env.JWT_AUTH_HEADER_PREFIX || "";
 
 export const GOOGLE_MAP_DEFAULT_POSITION = { lat: 55.755826, lng: 37.6172999 };
